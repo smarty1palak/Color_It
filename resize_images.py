58cb45d36e726ee84b3ad397b684ce3c54aed084
smarty1palak <smarty1palak@gmail.com>
2018-06-29 18:13:04 +0530

    convolution model for automatic colorisation

--- resize_images.py
+++ resize_images.py
@@ -2,13 +2,13 @@ from PIL import Image
 import os
 
 # All images in 'train_orig' will be resized to (dimension x dimension), and placed in 'train_resized' folder.
-dimension = 256
+dimension = 300
 
-if not os.path.exists('./train_resized'):
-    os.makedirs('./train_resized')
+if not os.path.exists('./result_resized'):
+    os.makedirs('./result_resized')
     
-for image in os.listdir('./train_orig/'):
-    im = Image.open('./train_orig/' + image)
+for image in os.listdir('./result/'):
+    im = Image.open('./result/' + image)
     im = im.resize((dimension, dimension))
-    im.save('./train_resized/' + image)
+    im.save('./result_resized/' + image)
     print('Resized', image)
